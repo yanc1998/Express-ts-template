bce5b0f521fb2115c3f87769b68b0b7889e00568
yank1 <yancarloglez98@gmail.com>
2022-03-10 13:51:47 -0500

    auth 1.1

--- src/base/core/ddd-base/core/interfaces/IEntity.ts
+++ src/base/core/ddd-base/core/interfaces/IEntity.ts
@@ -1,11 +1,4 @@
-import { IIdentifier } from '../../domain/Identifier'
 
 export type BaseProps = {
   [index: string]: unknown
-}
-
-export interface IEntity {
-  readonly _id: IIdentifier
-
-  equals(entity: IEntity): boolean
 }
